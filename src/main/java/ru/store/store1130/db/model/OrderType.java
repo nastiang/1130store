c0532e3e807f46dc866f6b8f499da9d4559c4910
Sway <Sway@swaypc>
2019-06-18 16:32:38 +0300

    fix

--- src/main/java/ru/store/store1130/db/model/OrderType.java
+++ src/main/java/ru/store/store1130/db/model/OrderType.java
@@ -1,6 +1,16 @@
 package ru.store.store1130.db.model;
 
 public enum OrderType {
-    PURCHASED,
-    RETURNED
+    PURCHASED("покупка"),
+    RETURNED("возврат");
+
+    private String text;
+
+    OrderType(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return this.text;
+    }
 }
